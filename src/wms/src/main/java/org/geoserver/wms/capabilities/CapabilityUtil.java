8ab40a3987781274f11fb75431aa47cd4a9a3184
Mauricio Pazos <mauricio.pazos@gmail.com>
2013-05-22 16:45:15 +0200

    scale hint computation

--- src/wms/src/main/java/org/geoserver/wms/capabilities/CapabilityUtil.java
+++ src/wms/src/main/java/org/geoserver/wms/capabilities/CapabilityUtil.java
@@ -70,6 +70,7 @@ final class CapabilityUtil {
 		    for (FeatureTypeStyle fts : style.featureTypeStyles()) {
 		    	
 		        for ( Rule rule : fts.rules() ) {
+		       
 		            if ( rule.getMinScaleDenominator() < scaleDenominator.get(minScaleDenominator) ) {
 		            	scaleDenominator.put(minScaleDenominator,  rule.getMinScaleDenominator());
 		            }
@@ -90,5 +91,14 @@ final class CapabilityUtil {
 		assert scaleDenominator.get(minScaleDenominator) <= scaleDenominator.get(maxScaleDenominator) : "Min <= Max scale is expected";
 
 	    return scaleDenominator;
+	}
+
+	public static Double computeScaleHint(final Double scaleDenominator) {
+		
+		final Double sizeStarndardRenderPixel = 0.0028;//(meters) 
+		
+		Double scaleHint = Math.sqrt(Math.pow((scaleDenominator * sizeStarndardRenderPixel), 2) * 2);
+		
+		return scaleHint;
 	}
 }
