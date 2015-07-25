e018ce31da70d7c79635ea97a07c513e72253385
Andrea Aime <andrea.aime@gmail.com>
2015-07-25 18:38:17 +0200

    [GEOS-7124] Remote Code Execution with Xstream

--- src/main/src/main/java/org/geoserver/config/util/XStreamPersister.java
+++ src/main/src/main/java/org/geoserver/config/util/XStreamPersister.java
@@ -34,6 +34,7 @@ import org.geoserver.catalog.CoverageInfo;
 import org.geoserver.catalog.CoverageStoreInfo;
 import org.geoserver.catalog.DataStoreInfo;
 import org.geoserver.catalog.FeatureTypeInfo;
+import org.geoserver.catalog.Info;
 import org.geoserver.catalog.Keyword;
 import org.geoserver.catalog.KeywordInfo;
 import org.geoserver.catalog.LayerGroupInfo;
@@ -262,10 +263,10 @@ public class XStreamPersister {
         ReflectionProvider reflectionProvider = new CustomReflectionProvider( new FieldDictionary( sorter ) ); 
             //new Sun14ReflectionProvider( new FieldDictionary( sorter  ) ); 
         if ( streamDriver != null ) {
-            xs = new XStream( reflectionProvider, streamDriver );
+            xs = new SecureXStream(reflectionProvider, streamDriver);
         }
         else {
-            xs = new XStream( reflectionProvider );    
+            xs = new SecureXStream(reflectionProvider);
         }
         xs.setMode(XStream.NO_REFERENCES);
         
@@ -440,6 +441,13 @@ public class XStreamPersister {
         registerBreifMapComplexType("dimensionInfo", DimensionInfoImpl.class);
         
         callback = new Callback();
+
+        // setup white list of accepted classes
+        xs.allowTypeHierarchy(Info.class);
+        xs.allowTypeHierarchy(Multimap.class);
+        xs.allowTypeHierarchy(JAIInfo.class);
+        xs.allowTypesByWildcard(new String[] { "org.geoserver.catalog.**" });
+        xs.allowTypesByWildcard(new String[] { "org.geoserver.security.**" });
     }
     
     /**
@@ -452,7 +460,7 @@ public class XStreamPersister {
     public void registerBreifMapComplexType(String typeId, Class clazz) {
         forwardBreifMap.put(typeId, clazz);
         backwardBreifMap.put(clazz, typeId);
-        
+        xs.allowTypes(new Class[] { clazz });
     }
 
     public XStream getXStream() {
