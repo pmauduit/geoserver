6a54f5b038181dffd721e7673356beb4fe0448a5
Niels Charlier <niels@scitus.be>
2015-11-11 14:15:55 +0100

    Layergroups: attribution & metadatalinks API+rest+getcapabilities
    Conflicts:
    	src/wms/src/test/java/org/geoserver/wms/wms_1_3/CapabilitiesIntegrationTest.java

--- src/main/src/main/java/org/geoserver/security/decorators/DecoratingLayerGroupInfo.java
+++ src/main/src/main/java/org/geoserver/security/decorators/DecoratingLayerGroupInfo.java
@@ -7,12 +7,14 @@ package org.geoserver.security.decorators;
 
 import java.util.List;
 
+import org.geoserver.catalog.AttributionInfo;
 import org.geoserver.catalog.AuthorityURLInfo;
 import org.geoserver.catalog.CatalogVisitor;
 import org.geoserver.catalog.LayerGroupHelper;
 import org.geoserver.catalog.LayerGroupInfo;
 import org.geoserver.catalog.LayerIdentifierInfo;
 import org.geoserver.catalog.LayerInfo;
+import org.geoserver.catalog.MetadataLinkInfo;
 import org.geoserver.catalog.MetadataMap;
 import org.geoserver.catalog.PublishedInfo;
 import org.geoserver.catalog.StyleInfo;
@@ -179,5 +181,20 @@ public class DecoratingLayerGroupInfo extends AbstractDecorator<LayerGroupInfo> 
     @Override
     public PublishedType getType() {
         return delegate.getType();
+    }
+
+    @Override
+    public AttributionInfo getAttribution() {
+        return delegate.getAttribution();
+    }
+
+    @Override
+    public void setAttribution(AttributionInfo attribution) {
+        delegate.setAttribution(attribution);
+    }
+
+    @Override
+    public List<MetadataLinkInfo> getMetadataLinks() {
+        return delegate.getMetadataLinks();
     }
 }
